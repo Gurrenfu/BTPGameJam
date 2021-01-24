05103004f55f7d98f47e37709291d27abd73b9ae
aGlymph <adamglymph@gmail.com>
2021-01-24 20:07:24 +0900

    no message

---
+++ BTPGJ/Assets/Scripts/GameController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text displayText;
+    public InputAction[] inputActions;
+    [HideInInspector] public RoomNavigation roomNavigation;
+    [HideInInspector] public List<string> ineractionDescriptionsInRoom = new List<string>();
+    [HideInInspector] public InteractableItems interactableItems;
+    List<string> actionLog = new List<string>();
+
+    
+    void Awake()
+    {
+        interactableItems = GetComponent<InteractableItems>();
+        roomNavigation = GetComponent<RoomNavigation>();
+    }
+
+    private void Start()
+    {
+        DisplayRoomText();
+        DisplayLoggedText();
+    }
+
+    public void DisplayLoggedText()
+    {
+        string logAsText = string.Join("\n", actionLog.ToArray());
+        displayText.text = logAsText;
+    }
+
+    public void DisplayRoomText()
+    {
+        ClearCollectionsForNewRoom();
+        UnpackRoom();
+        string joinedInteractionDescriptions = string.Join("\n", ineractionDescriptionsInRoom.ToArray());
+        string combinedText = roomNavigation.currentRoom.Description + "\n" + joinedInteractionDescriptions;
+        LogStringWithReturn(combinedText);
+    }
+
+    private void UnpackRoom()
+    {
+        roomNavigation.UnpackExitsInRoom();
+        PrepareObjectToTakeOrExamine(roomNavigation.currentRoom);
+    }
+
+    private void PrepareObjectToTakeOrExamine(Room currentRoom)
+    {
+        for (int i = 0; i < currentRoom.interactableObjectsInRoom.Length; i++)
+        {
+            string descriptionNotInInventory = interactableItems.GetObjectsNotInInvetory(currentRoom, i);
+            if(descriptionNotInInventory != null)
+            {
+                ineractionDescriptionsInRoom.Add(descriptionNotInInventory);
+            }
+
+            InteractableObject interactableInRoom = currentRoom.interactableObjectsInRoom[i];
+
+            for (int j = 0; j < interactableInRoom.ineractions.Length; j++)
+            {
+                Interaction interaction = interactableInRoom.ineractions[j];
+                if(interaction.inputAction.keyWord == "examine")
+                {
+                    interactableItems.examineDictionary.Add(interactableInRoom.noun, interaction.textResponse);                
+                }
+
+                if (interaction.inputAction.keyWord == "take")
+                {
+                    interactableItems.takeDictionary.Add(interactableInRoom.noun, interaction.textResponse);
+                }
+            }
+        }
+    }
+
+    public string TestVerbDictionaryWithNoun(Dictionary<string, string> verbDictionary, string verb, string noun)
+    {
+        if(verbDictionary.ContainsKey(noun))
+        {
+            return verbDictionary[noun];
+        }
+        return "You can't " + verb + " " + noun;
+    }
+
+    void ClearCollectionsForNewRoom()
+    {
+        interactableItems.ClearCollections();
+        ineractionDescriptionsInRoom.Clear();
+        roomNavigation.ClearExits();
+    }
+
+    public void LogStringWithReturn(string stringtoAdd)
+    {
+        actionLog.Add(stringtoAdd + "\n");
+    }
+
+    void Update()
+    {
+        
+    }
+}
